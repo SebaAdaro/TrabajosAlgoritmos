93dfb9618b3cca58acabcbd9f104de70a759d411
Beltran <beltran.bulbarella@ing.austral.edu.ar>
2021-03-15 00:40:44 -0300

    a

--- src/ComparacionDeAlgoritmo/Ej2/ArregloDeEnterosRandom.java
+++ src/ComparacionDeAlgoritmo/Ej2/ArregloDeEnterosRandom.java
@@ -5,16 +5,18 @@ import Util.Scanner;
 //Generar en forma random un arreglo de números enteros para distintos valores de N
 //(siendo N el tamaño del arreglo) y comparar los tiempos para distintos valores de N.
 public class ArregloDeEnterosRandom {
+
     int n;
-    static int[] myArray;
     public static void randomArray(int n){
+        int[] myArray = new int[n];
         for (int i = 0; i < n; i++) {
-            myArray[i] = ((int)Math.random()*100);
+            myArray[i] = ((int)(Math.random()*10));
+            System.out.println(myArray[i]);
         }
     }
 
     public static void main(String[] args) {
         int n = Scanner.getInt("Ingrese su tamano");
-
+        randomArray(n);
     }
 }
