4130c631816737e2871f30d901ca959ca3940731
rjosue6 <josue.riera@ing.austral.edu.ar>
2021-03-15 00:58:05 -0300

    a

--- src/ComparacionDeAlgoritmo/Ej2/InsertionSort.java
+++ src/ComparacionDeAlgoritmo/Ej2/InsertionSort.java
@@ -1,6 +1,35 @@
 package ComparacionDeAlgoritmo.Ej2;
 
-public class InsertionSort<T extends Comparable<? super T>> {
+public class InsertionSort {
+
+    public static void insercion(int a[]){
+        for (int i = 0; i < a.length; i++) {
+            int pos = i;
+            int aux = a[i];
+            while((pos > 0) && (a[pos-1] > aux)){
+                a[pos] = a[pos-1];
+                pos--;
+            }
+            a[pos] = aux;
+        }
+    }
+
+    public static void insercionCadena(String a[]){
+        for (int i = 0; i < a.length; i++) {
+            int pos = i;
+            String aux = a[i];
+            while((pos > 0) && (a[pos-1].compareTo(aux) > 0)){
+                a[pos] = a[pos-1];
+                pos--;
+            }
+            a[pos] = aux;
+        }
+    }
+}
+
+
+
+/*public class InsertionSort<T extends Comparable<? super T>> {
 
     void insertionSort(T[] array)
     {
@@ -14,7 +43,7 @@ public class InsertionSort<T extends Comparable<? super T>> {
              *      2. that the item directly before the currentIndex is greater than the item at currentIndex
              *
              * If both conditions are met, swap the indexes
-             */
+
             while (currentIndex > 0 && array[currentIndex - 1].compareTo(array[currentIndex]) > 0)
             {
                 T temp = array[currentIndex];
@@ -25,4 +54,6 @@ public class InsertionSort<T extends Comparable<? super T>> {
         }
     }
 }
+
+*/
 
