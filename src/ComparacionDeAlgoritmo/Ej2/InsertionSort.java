78d794fefc1841d04ceda28593f0a6cab94246fb
rjosue6 <josue.riera@ing.austral.edu.ar>
2021-03-15 01:23:05 -0300

    a

--- src/ComparacionDeAlgoritmo/Ej2/InsertionSort.java
+++ src/ComparacionDeAlgoritmo/Ej2/InsertionSort.java
@@ -1,8 +1,8 @@
 package ComparacionDeAlgoritmo.Ej2;
 
-public class InsertionSort {
+public class InsertionSort<T extends Comparable < ? super T>> {
 
-    public static void insercion(int a[]){
+    public static void insertion(int a[]){
         for (int i = 0; i < a.length; i++) {
             int pos = i;
             int aux = a[i];
@@ -14,7 +14,7 @@ public class InsertionSort {
         }
     }
 
-    public static void insercionCadena(String a[]){
+    public static void insertionCadena(String a[]){
         for (int i = 0; i < a.length; i++) {
             int pos = i;
             String aux = a[i];
@@ -25,35 +25,18 @@ public class InsertionSort {
             a[pos] = aux;
         }
     }
-}
 
-
-
-/*public class InsertionSort<T extends Comparable<? super T>> {
-
-    void insertionSort(T[] array)
-    {
-        // start at the first index and iterate through to the end
-        for (int i = 1; i < array.length; i++)
-        {
-            int currentIndex = i;
-            /*
-             * Check:
-             *      1. that currentIndex is at least 1
-             *      2. that the item directly before the currentIndex is greater than the item at currentIndex
-             *
-             * If both conditions are met, swap the indexes
-
-            while (currentIndex > 0 && array[currentIndex - 1].compareTo(array[currentIndex]) > 0)
-            {
-                T temp = array[currentIndex];
-                array[currentIndex] = array[currentIndex - 1];
-                array[currentIndex - 1] = temp;
-                currentIndex--;
+    public void insertionObjetoT(T[] a){
+        for (int i = 0; i < a.length; i++) {
+            int pos = i;
+            T aux = a[i];
+            while((pos > 0) && (a[pos-1].compareTo(aux) > 0)){
+                a[pos] = a[pos-1];
+                pos--;
             }
+            a[pos] = aux;
         }
     }
-}
 
-*/
+}
 
