d534a95c631f18162b4be4f77450f6db14b14741
rjosue6 <josue.riera@ing.austral.edu.ar>
2021-03-15 21:09:08 -0300

    aa

--- src/ComparacionDeAlgoritmo/Ej2/InsertionSort.java
+++ src/ComparacionDeAlgoritmo/Ej2/InsertionSort.java
@@ -2,7 +2,7 @@ package ComparacionDeAlgoritmo.Ej2;
 
 public class InsertionSort<T extends Comparable < ? super T>> {
 
-    public static void insertion(int a[]){
+    public static void insertion(int [] a){
         for (int i = 0; i < a.length; i++) {
             int pos = i;
             int aux = a[i];
@@ -14,7 +14,7 @@ public class InsertionSort<T extends Comparable < ? super T>> {
         }
     }
 
-    public static void insertionCadena(String a[]){
+    public static void insertionCadena(String[] a){
         for (int i = 0; i < a.length; i++) {
             int pos = i;
             String aux = a[i];
@@ -26,24 +26,11 @@ public class InsertionSort<T extends Comparable < ? super T>> {
         }
     }
 
-    public <T> void insertionObjetoT(T[] a, Comparable<T> k){
-        for (int i = 0; i < a.length; i++) {
-            int pos = i;
-            T aux = a[i];
-            while((pos > 0) && (k.compareTo((T) a[pos-1]) < 0)){
-                a[pos] = a[pos-1];
-                pos--;
-            }
-            a[pos] = aux;
-        }
-    }
-
-
     public void insertionObjetoT(T[] a){
         for (int i = 0; i < a.length; i++) {
             int pos = i;
             T aux = a[i];
-            while((pos > 0) && (a[pos-1].compareTo(aux) < 0)){
+            while((pos > 0) && ((a[pos-1]).compareTo(aux) < 0)){
                 a[pos] = a[pos-1];
                 pos--;
             }
