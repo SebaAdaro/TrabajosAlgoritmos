a4fed1f7c66952f77c04fda4d36f24f92c082d4b
rjosue6 <josue.riera@ing.austral.edu.ar>
2021-03-17 03:03:09 -0300

    Tp2

--- src/ComparacionDeAlgoritmo/Ej3/MergeAlgorithm.java
+++ src/ComparacionDeAlgoritmo/Ej3/MergeAlgorithm.java
@@ -2,12 +2,12 @@
 
 package ComparacionDeAlgoritmo.Ej3;
 
-public class MergeAlgorithm{
+public class MergeAlgorithm<T extends Comparable<? super T>> {
 
-    public void mergeArrays(int[] a, int[] b, int var1, int var2, int[] c) {
+    public void mergeArraysGeneric(T[] a, T[] b, int var1, int var2, T[] c) {
         int i = 0, j = 0, k = 0;
         while (i < var1 && j < var2) {
-            if (a[i] < b[j])
+            if (a[i].compareTo(b[j]) < 0)
                 c[k++] = a[i++];
             else
                 c[k++] = b[j++];
@@ -16,28 +16,5 @@ public class MergeAlgorithm{
             c[k++] = a[i++];
         while (j < var2)
             c[k++] = b[j++];
-    }
-
-    public static void main(String[] args) {
-        int[] a = {10,20,30,40};
-        int var1 = a.length;
-
-        int[] b = {5,15,25,35};
-        int var2 = b.length;
-
-        int[] c = new int[var1 + var2];
-
-        MergeAlgorithm mergeAlgorithm = new MergeAlgorithm();
-        mergeAlgorithm.mergeArrays(a, b, var1, var2, c);
-
-        printArray(c);
-    }
-
-    public static void printArray(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n; ++i) {
-            System.out.print(arr[i] + " ");
-            System.out.println();
-        }
     }
 }
