f2bd50575e0b19bcfa5a514b0138fa1349c5363d
Beltran <beltran.bulbarella@ing.austral.edu.ar>
2021-03-30 14:51:10 -0300

    a

---
+++ src/ArbolesBinarios/Ejercicio14.java
@@ -0,0 +1,19 @@
+package ArbolesBinarios;
+/*a) Se dispone de un árbol binario de elementos de tipo entero. Escribir métodos que calculen:
+        i) la suma de sus elementos.
+        ii) la suma de sus elementos que son múltiplos de 3*/
+
+    public class Ejercicio14<T> {
+
+        public int elementSum(BinaryTree<Integer> a){
+            if (a.isEmpty()){
+                return 0;
+            }
+            int sum = 0;
+            sum += a.getRoot();
+
+            return 0;
+
+        }
+
+}
