ec2c0a007d77fa401a9f071657aefae6d5fcdc48
sebaa <sebastian.adaro@ing.austral.edu.ar>
2021-03-30 13:55:37 -0300

    ejercicio 13

---
+++ src/ArbolesBinarios/Ejercicio13.java
@@ -0,0 +1,43 @@
+package ArbolesBinarios;
+
+public class Ejercicio13<T> {
+
+    public int size(BinaryTree<T> a) {
+        if (a.isEmpty())
+            return 0;
+        else
+            return 1 + size(a.getLeft()) + size(a.getRight());
+    }
+
+    public int leavesAux(BinaryTree a, int count){
+        int counter = count;
+        if (a.isEmpty()) {
+            counter++;
+        }
+        if (!a.getLeft().isEmpty() && !a.getRight().isEmpty()) {
+            return leavesAux(a.getLeft(),counter) + leavesAux(a.getRight(),counter);
+        }
+        return counter;
+    }
+
+    public int leaves(BinaryTree<T> a) {
+        return leavesAux(a,0);
+    }
+
+    public  int ocurrencias(BinaryTree<T> a, T o){
+        if(a.isEmpty())
+            return 0;
+        if(a.getRoot().equals(o))
+            return
+                    1 + ocurrencias(a.getLeft(),o)+ocurrencias(a.getRight(),o);
+        else
+            return ocurrencias(a.getLeft(),o)+ocurrencias(a.getRight(),o);
+    }
+
+    public int elementsInLevel(BinaryTree<T> a, int b){
+        return 0;
+    }
+
+
+
+}
