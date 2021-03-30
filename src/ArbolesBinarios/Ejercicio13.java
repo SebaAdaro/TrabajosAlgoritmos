04887ee4f144bb84cabb71976d8607f895f4e9b1
sebaa <sebastian.adaro@ing.austral.edu.ar>
2021-03-30 14:16:51 -0300

    ejercicio 13 altura

--- src/ArbolesBinarios/Ejercicio13.java
+++ src/ArbolesBinarios/Ejercicio13.java
@@ -2,6 +2,8 @@ package ArbolesBinarios;
 
 public class Ejercicio13<T> {
 
+    private int height;
+
     public int size(BinaryTree<T> a) {
         if (a.isEmpty())
             return 0;
@@ -9,35 +11,50 @@ public class Ejercicio13<T> {
             return 1 + size(a.getLeft()) + size(a.getRight());
     }
 
-    public int leavesAux(BinaryTree a, int count){
+    public int leavesAux(BinaryTree a, int count) {
         int counter = count;
         if (a.isEmpty()) {
             counter++;
         }
         if (!a.getLeft().isEmpty() && !a.getRight().isEmpty()) {
-            return leavesAux(a.getLeft(),counter) + leavesAux(a.getRight(),counter);
+            return leavesAux(a.getLeft(), counter) + leavesAux(a.getRight(), counter);
         }
         return counter;
     }
 
     public int leaves(BinaryTree<T> a) {
-        return leavesAux(a,0);
+        return leavesAux(a, 0);
     }
 
-    public  int ocurrencias(BinaryTree<T> a, T o){
-        if(a.isEmpty())
+    public int ocurrencias(BinaryTree<T> a, T o) {
+        if (a.isEmpty())
             return 0;
-        if(a.getRoot().equals(o))
+        if (a.getRoot().equals(o))
             return
-                    1 + ocurrencias(a.getLeft(),o)+ocurrencias(a.getRight(),o);
+                    1 + ocurrencias(a.getLeft(), o) + ocurrencias(a.getRight(), o);
         else
-            return ocurrencias(a.getLeft(),o)+ocurrencias(a.getRight(),o);
+            return ocurrencias(a.getLeft(), o) + ocurrencias(a.getRight(), o);
     }
 
-    public int elementsInLevel(BinaryTree<T> a, int b){
+    public int elementsInLevel(BinaryTree<T> a, int b) {
         return 0;
     }
 
+    public int height(BinaryTree<T> a) {
+        height = 0;
+        heightAux(a, 1);
+        return height;
+    }
+
+    private void heightAux(BinaryTree<T> a, int i) {
+        if (!a.isEmpty()) {
+            heightAux(a.getLeft(), i + 1);
+            if (i > height) {
+                height = i;
+            }
+            heightAux(a.getRight(), i + 1);
+        }
+    }
 
 
 }
