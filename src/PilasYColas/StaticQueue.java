a3cc705cf29f33554d54526f365e4646af0cc42d
sebaa <sebastian.adaro@ing.austral.edu.ar>
2021-04-14 20:08:58 -0300

    static queue

--- src/PilasYColas/StaticQueue.java
+++ src/PilasYColas/StaticQueue.java
@@ -3,36 +3,33 @@ package PilasYColas;
 
 public class StaticQueue<T> implements QueueInterface<T> {
     private T[] array;
-    private int front, size, back;
-
+    private int front;
+    private int back;
+    private int size;
 
     public StaticQueue() {
         array = (T[]) new Object[10];
-        front = 0;
-        back = 0;
-        size = 0;  //todo change amount for size
+        front = -1;
+        back = -1;
+        size = 0;
     }
 
     @Override
     public void enqueue(T element) {
-        if (!isFull()) {
-            size++;
-            array[back - 1] = element;
+        if (isEmpty()) {
+            back = 0;
+            front = 0;
         } else {
-            grow();
-            enqueue(element);
+            manageIndex(back, front);
+            ++back;
         }
+        array[back] = element;
+        ++size;
     }
 
-    private boolean isFull() {
-        return array.length == size;
-    }
-
-    private void increment() {
-        if (back != array.length) {
-            back++;
-        } else if (size < array.length) {
-            back = 1;
+    private void manageIndex(int index, int choice) {
+        if (back + 1 == size || back + 1 == front) {
+            enlargeArray();
         }
     }
 
@@ -40,11 +37,18 @@ public class StaticQueue<T> implements QueueInterface<T> {
     public T dequeue() throws IsEmptyException {
         if (!isEmpty()) {
             T temp = array[front];
-            front++;
-            size++;
+            manageFront();
+            ++front;
+            --size;
             return temp;
         } else {
             throw new IsEmptyException();
+        }
+    }
+
+    private void manageFront() {
+        if (front + 1 == array.length) {
+            front = 0;
         }
     }
 
@@ -58,18 +62,16 @@ public class StaticQueue<T> implements QueueInterface<T> {
         return size;
     }
 
-    private void grow() { //cambiar para que lo copie en orden
-        T[] newArray = (T[]) new Object[array.length * 2];
-        int index = 0;
-        for (int i = front; i < array.length; i++) {
-            newArray[index++] = array[i];
+    private void enlargeArray() {
+        T[] array1 = array;
+        array = (T[]) new Object[array1.length + 5];
+        for (int i = 0; i < array1.length; i++) {
+            array[i] = array1[front];
+            if (front != array1.length - 1) {
+                ++front;
+            } else {
+                front = 0;
+            }
         }
-        for (int i = back - 1; i < front; i++) {
-            newArray[index++] = array[i];
-        }
-        front = 0;
-        back = array.length;
-        array = newArray;
     }
 }
-
